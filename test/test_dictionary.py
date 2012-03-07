68c0c3170acc1ebad42e331f294ecdd594e7535c
Chris Montrois <chris@chrismontrois.net>
2012-03-07 00:42:21 -0700

    Renamed Word_Finder to WordFinder

--- test/test_dictionary.py
+++ test/test_dictionary.py
@@ -5,11 +5,13 @@ from scrabbler.dictionary import loader
 class Test(unittest.TestCase):
 
 	def test_load(self):
+		print "Starting load..."
 		loader.load()
+		print "...Finished"
 
 	# def test_match(self):
 	# 	loader.load()
-	# 	w = word_finder.Word_finder()
+	# 	w = word_finder.WordFinder()
 
 	# 	matches = w.get_words('tooth', [("h", 4)])
 	# 	print matches
