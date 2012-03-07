6a099a88dbb84adce57470c0d88ab3e61a6d9c9c
Chris Montrois <chris@chrismontrois.net>
2012-03-07 00:40:40 -0700

    Whitespace changes

--- scrabbler/dictionary/__init__.py
+++ scrabbler/dictionary/__init__.py
@@ -2,13 +2,13 @@ import config
 from words import Words
 
 def is_key(key):
-    return key in Words.hashed
+	return key in Words.hashed
 
 def is_word(word):
-    return word in Words.words
+	return word in Words.words
 
 def get_words(key):
-    return Words.hashed[key]
+	return Words.hashed[key]
 
 def get_letter_value(letter):
-    return config.letters[letter].value
+	return config.letters[letter].value
